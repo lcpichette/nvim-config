0868efc546a4ae4b4a7b4954cb5d11f647e06504
Lucas Pichette <lucascpichette@gmail.com>
2024-09-27 01:28:18 -0700

    removes a lot of mappings done in user files in favor of better-practice AstroVim mapping

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,98 @@
+local harpoon = require "harpoon"
+
+harpoon:setup()
+harpoon:setup {}
+
+-- basic telescope configuration
+local conf = require("telescope.config").values
+
+local function toggle_telescope(harpoon_files)
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table {
+        results = file_paths,
+      },
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+    })
+    :find()
+end
+
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- SYSTEM
+          ["<leader>rn"] = {
+            function() vim.wo.relativenumber = not vim.wo.relativenumber end,
+            desc = "Toggle relative line numbers",
+          },
+          -- HARPOON
+          ["<Leader>H"] = { desc = "Harpoon" },
+          ["<Leader>Ha"] = {
+            function() harpoon:list():add() end,
+            desc = "Add new harpoon target",
+          },
+          ["<Leader>Hl"] = {
+            function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+            desc = "List harpoon targets",
+          },
+          ["<Leader>Ht"] = {
+            function() toggle_telescope(harpoon:list()) end,
+            desc = "Open harpoon window",
+          },
+          -- BUFFER
+          ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+          ["<Leader>bD"] = {
+            function()
+              require("astroui.status").heirline.buffer_picker(
+                function(bufnr) require("astrocore.buffer").close(bufnr) end
+              )
+            end,
+            desc = "Pick to close",
+          },
+          -- tables with just a `desc` key will be registered with which-key if it's installed
+          -- this is useful for naming menus
+          ["<Leader>b"] = { desc = "Buffers" },
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          K = {
+            function() vim.lsp.buf.hover() end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function() vim.lsp.buf.declaration() end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
+}
